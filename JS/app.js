10ca8d3f1744370b29700e89cd2a941cfac72595
NahuDiaz3699 <naahuediaz@hotmail.com>
2024-10-10 18:24:00 -0300

    proyecto final

---
+++ JS/app.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    Swal.fire({
+        title: 'Bienvenido a la página de contacto!',
+        text: 'Estamos aquí para ayudarte. ¿En qué podemos asistirte hoy?',
+        icon: 'info',
+        confirmButtonText: 'Aceptar'
+    });
+});
