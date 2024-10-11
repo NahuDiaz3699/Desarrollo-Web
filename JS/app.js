c865472b25c531649b19e58fb2e2fafec622905f
NahuDiaz3699 <naahuediaz@hotmail.com>
2024-10-10 22:55:38 -0300

    Fin del proyecto final

--- JS/app.js
+++ JS/app.js
@@ -1,16 +1,1 @@
-// Verificar si el usuario ya ha visto el SweetAlert
-if (!localStorage.getItem('alertShown')) {
-    // Mostrar SweetAlert si no ha sido mostrado antes
-    Swal.fire({
-        title: '¡Bienvenido a la página de Contacto!',
-        text: 'Nos alegra que estés aquí. Si tienes alguna consulta, no dudes en escribirnos.',
-        icon: 'info',
-        confirmButtonText: 'Entendido',
-        customClass: {
-            popup: 'my-custom-popup'
-        }
-    });
 
-    // Guardar en localStorage para que no se muestre nuevamente
-    localStorage.setItem('alertShown', 'true');
-}
